c4ba7ee41a940c4a191f3dab69aab2d0482ad196
Mahmud <55327257+Mahmud2640@users.noreply.github.com>
2024-10-08 11:57:40 +0600

    header file created

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <div>
-      <h1>This Is End</h1>
-    </div>
-  );
+  return <div>Footer</div>;
 };
 
 export default Footer;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,56 @@
 import React from "react";
+import { Container, Row, Button } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import "./header.css";
+
+const nav_links = [
+  { path: "/home", display: "Home" },
+  { path: "/tour", display: "Tour" },
+  { path: "/#", display: "About" },
+];
 
 const Header = () => {
   return (
-    <div>
-      <h1> My name is Nur Alam</h1>
-    </div>
+    <header>
+      <Container>
+        <Row>
+          {/* Logo */}
+          <div className="nav_wrapper d-flex align-items-center justify-content-between">
+            <div className="logo">
+              <img src={logo} alt="logo_image" />
+            </div>
+
+            {/* Nagigation */}
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {nav_links.map((item, index) => (
+                  <li className="nav_item" key={index}>
+                    <NavLink to={item.path}>{item.display}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Button */}
+            <div className="nav_right d-flex align-items-center gap-4">
+              <div className="nav_btns d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+
+              <span className="mobile_menu">
+                <i class="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
   );
 };
 
