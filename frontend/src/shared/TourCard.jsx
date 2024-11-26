ec92bc8d04a1af8c2adc45faa496308b2f1924fe
Mahmud <55327257+Mahmud2640@users.noreply.github.com>
2024-11-26 10:11:24 +0600

    Refactor TourCard component structure and update styles in tour-card.css

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -13,29 +13,33 @@ const TourCard = ({ tour }) => {
           <img src={photo} alt="tour-img" />
           <span>Featured</span>
         </div>
-      </Card>
-      <CardBody>
-        <div className="card_top d-flex align-items-center justify-content-between">
-          <span className="tour_location d-flex align-items-center gap-1">
-            <i className="ri-map-pin-line"></i> {city}
-          </span>
-          <span className="tour_rating d-flex align-items-center gap-1">
-            <i className="ri-star-fill"></i> {avgRating}{" "}
-            <span>({reviews.length})</span>
-          </span>
-        </div>
-        <h5 className="tour_title">
-          <Link to={`/tours/${id}`}>{title}</Link>
-        </h5>
-        <div className="card_bottom d-flex align-items-center justify-content-between mt-3">
-          <h5>
-            ${price} <span> /per person</span>
+
+        <CardBody>
+          <div className="card_top d-flex align-items-center justify-content-between">
+            <span className="tour_location d-flex align-items-center gap-1">
+              <i className="ri-map-pin-line"></i> {city}
+            </span>
+            <span className="tour_rating d-flex align-items-center gap-1">
+              <i className="ri-star-fill"></i> {avgRating}{" "}
+              <span>({reviews.length})</span>
+            </span>
+          </div>
+
+          <h5 className="tour_title">
+            <Link to={`/tours/${id}`}>{title}</Link>
           </h5>
-          <button className="btn booking_btn">
-            <Link to={`/tours/${id}`}>Book Now</Link>
-          </button>
-        </div>
-      </CardBody>
+
+          <div className="card_bottom d-flex align-items-center justify-content-between mt-3">
+            <h5>
+              ${price} <span> /per person</span>
+            </h5>
+
+            <button className="btn booking_btn">
+              <Link to={`/tours/${id}`}>Book Now</Link>
+            </button>
+          </div>
+        </CardBody>
+      </Card>
     </div>
   );
 };
